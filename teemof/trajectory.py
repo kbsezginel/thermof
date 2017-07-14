78034941482923fa5cd8dd53f1c7eeb612278452
kbsezginel <kbs37@pitt.edu>
2017-07-14 13:07:06 -0400

    Add method to ready xyz trajectory files

--- teemof/trajectory.py
+++ teemof/trajectory.py
@@ -30,3 +30,23 @@ def change_xyz_trajectories(trial_dir, name='traj.xyz', atoms=['C', 'O'], target
             print('Trajectory: %s' % run) if verbose else None
             xyz_traj_path = os.path.join(trial_dir, run, name)
             change_xyz_atom_names(xyz_traj_path, atoms=atoms, targets=targets)
+
+
+def read_trajectory(traj_path):
+    """ Read xyz traectory and return coordinates as a list """
+    with open(traj_path, 'r') as t:
+        traj = t.readlines()
+
+    n_atoms = int(traj[0].strip())                # Get nmber of atoms from first line
+    n_frames = int(len(traj) / (n_atoms + 2))     # Calculate number of frames (assuming n_atoms is constant)
+
+    trajectory = {'atoms': [], 'coordinates': [], 'xyz': [], 'timestep': []}
+    for frame in range(n_frames):
+        start = frame * (n_atoms + 2)             # Frame start
+        end = (frame + 1) * (n_atoms + 2) - 1     # Frame end
+        trajectory['xyz'].append(traj[start:end])
+        trajectory['timestep'].append(traj[start + 1].strip().split()[2])
+        trajectory['atoms'].append([line.split()[0] for line in traj[start + 2:end]])
+        trajectory['coordinates'].append([[float(i) for i in line.split()[1:4]] for line in traj[start + 2:end]])
+
+    return trajectory
