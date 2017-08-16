feba21724e30198cb59999d189c5ee60db7acfa7
kbsezginel <kbs37@pitt.edu>
2017-08-16 15:53:29 -0400

    Initialize simulation class

---
+++ teemof/simulation.py
@@ -0,0 +1,19 @@
+# Date: August 2017
+# Author: Kutay B. Sezginel
+"""
+Simulation class for reading and initializing Lammps simulations
+"""
+
+
+class Simulation:
+    """
+    Reading and initializing Lammps simulations
+    """
+    def __init__(self):
+        pass
+
+    def read(self):
+        pass
+
+    def initialize(self):
+        pass
