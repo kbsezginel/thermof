12e67f389c54f2ee5b707e4947b4ad8df9dd30e2
kbsezginel <kbs37@pitt.edu>
2017-08-19 17:44:19 -0400

    Add readInfo and read_thermo tests for simulation

--- tests/test_simulation_read.py
+++ tests/test_simulation_read.py
@@ -13,6 +13,8 @@ k_ref_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'thermal-c
 time_ref_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'time.yaml')
 trial_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'ideal-mof-trial')
 trial_set_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'ideal-mof-trial-set')
+run_info_ref_file = os.path.join(trial_dir, 'Run1', 'run_info.yaml')
+thermo_ref_file = os.path.join(trial_dir, 'Run1', 'thermo.yaml')
 k_est_iso_ref = [0.8624217134742657, 0.6839092609282974, 0.9263423943319228, 0.8656413422445915, 0.8983945996223535,
                  0.8802163796582159, 0.6416173216846418, 0.8356379755434158, 0.7098404203275488, 0.8686063495516347]
 
@@ -20,11 +22,19 @@ k_est_iso_ref = [0.8624217134742657, 0.6839092609282974, 0.9263423943319228, 0.8
 def test_simulation_read_run():
     """Test Simulation class read method for reading a single run with read_run"""
     k_par = k_parameters.copy()
+    k_par['read_thermo'] = True
+    k_par['read_info'] = True
     run_dir = os.path.join(trial_dir, 'Run1')
     run_data = read_run(run_dir, k_par=k_par)
     sim = Simulation(read=run_dir, parameters=k_par)
     sim.read(run_dir, setup='run')
+    with open(run_info_ref_file, 'r') as riref:
+        run_info_ref = yaml.load(riref)
+    with open(thermo_ref_file, 'r') as tref:
+        thermo_ref = yaml.load(tref)
     assert sim.run == run_data
+    assert sim.run['info'] == run_info_ref
+    assert sim.run['thermo'] == thermo_ref
 
 
 def test_simulation_read_trial():
