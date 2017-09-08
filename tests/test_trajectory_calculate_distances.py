84e9131b385f50e27bd8fa98fd0de832893e430b
kbsezginel <kbs37@pitt.edu>
2017-09-08 12:29:36 -0400

    Add calculate distance test for nonzero ref frame

--- tests/test_trajectory_calculate_distances.py
+++ tests/test_trajectory_calculate_distances.py
@@ -23,3 +23,19 @@ def test_trajectory_calculate_distances(max_dist=5):
     for frame in traj.distances:
         assert np.all(frame < max_dist)
     assert np.isclose(traj.distances[3][3], 0.5159079779340517)
+
+
+def test_trajectory_calculate_distances_nonzero_reference_frame(max_dist=5):
+    """
+    Tests interatomic distance calculation for the Trajectory class
+        - Test seventh frame (reference frame) is all zeros
+        - Make sure all distances are less than max distance (5 A)
+        - Test a known distance
+    """
+    traj = Trajectory(read=os.path.join(mof_trial_dir, 'Run1', 'traj.xyz'))
+    traj.set_cell([80, 80, 80])
+    traj.calculate_distances(reference_frame=7)
+    assert np.allclose(traj.distances[7], np.zeros(traj.n_atoms))
+    for frame in traj.distances:
+        assert np.all(frame < max_dist)
+    assert np.isclose(traj.distances[0][3], 0.23030113908032163)
