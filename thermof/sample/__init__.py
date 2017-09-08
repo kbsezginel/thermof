705d0b53fd5906891c025685a714e0cfb95e1afa
kbsezginel <kbs37@pitt.edu>
2017-09-08 11:01:37 -0400

    Add functions to load sample traj and sim objects

--- thermof/sample/__init__.py
+++ thermof/sample/__init__.py
@@ -2,6 +2,8 @@
 Sample input files for predicting thermal conductivity of porous crystals using Lammps
 """
 import os
+from thermof import Simulation, Trajectory
+from thermof.parameters import k_parameters
 
 
 sample_dir = os.path.abspath(os.path.dirname(__file__))
@@ -18,3 +20,32 @@ qsub_path = os.path.join(sample_dir, 'lammps_qsub.sh.sample')
 
 samples = dict(ideal_mof=dict(inp=single_inp3_path, data=single_data_path, qsub=qsub_path),
                ideal_interpenetrated_mof=dict(inp=ipmof_inp3_path, data=ipmof_data_path, qsub=qsub_path))
+
+
+tests_dir = os.path.join(sample_dir, '..', '..', 'tests')
+
+
+def load_sample_simulation(mof='single', setup='run', tests_dir=tests_dir, parameters=k_parameters.copy()):
+    """ Load a sample simulation object from the tests directory """
+    if mof == 'single':
+        trial = os.path.join(tests_dir, 'ideal-mof-trial')
+        if setup == 'run':
+            sample_simulation = Simulation(read=os.path.join(trial, 'Run1'), setup='run', parameters=parameters)
+        elif setup == 'trial':
+            sample_simulation = Simulation(read=trial, setup='trial', parameters=parameters)
+    else:
+        trial = os.path.join(tests_dir, 'ip-mof-trial')
+        if setup == 'run':
+            sample_simulation = Simulation(read=os.path.join(trial, 'Run1'), setup='run', parameters=parameters)
+        elif setup == 'trial':
+            sample_simulation = Simulation(read=trial, setup='trial', parameters=parameters)
+    return sample_simulation
+
+
+def load_sample_trajectory(mof='single', tests_dir=tests_dir):
+    """ Load a sample trajectory file from tests directory """
+    if mof == 'single':
+        sample_traj = Trajectory(read=os.path.join(tests_dir, 'ideal-mof-trial', 'Run1', 'traj.xyz'))
+    else:
+        sample_traj = Trajectory(read=os.path.join(tests_dir, 'ip-mof-trial', 'Run1', 'traj.xyz'))
+    return sample_traj
