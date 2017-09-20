a418d9efd3ddcdafe31c17a88d11adf04c6f9ab9
kbsezginel <kbs37@pitt.edu>
2017-09-20 16:51:22 -0400

    Add functions for writing job submission files from parameters.:

--- thermof/initialize.py
+++ thermof/initialize.py
@@ -6,6 +6,7 @@ Initialize Lammps input files of thermal conductivity measurements
 import os
 import yaml
 from thermof.sample import samples, thermal_flux_file
+from thermof.sample import slurm_file, pbs_file
 
 
 def get_files(sample_files=samples['ideal_interpenetrated_mof']):
@@ -82,16 +83,44 @@ def change_masses(data_lines, masses):
     return new_lines
 
 
-def lammps_qsub(qsub_lines, name='Lammps', walltime='12:00:00', nodes=1, ppn=4, queue='shared'):
-    """ Genereate qsub file for Lammps """
-    new_lines = qsub_lines[:3]
-    new_lines += ['#PBS -N %s\n' % name]
-    new_lines += ['#PBS -q %s\n' % queue]
-    new_lines += ['#PBS -l nodes=%i:ppn=%i\n' % (nodes, ppn)]
-    new_lines += ['#PBS -l walltime=%s\n' % walltime]
-    new_lines += qsub_lines[7:]
+def job_submission_file(file_name, parameters):
+    """ Generate job submission file from given parameters """
+    if parameters.scheduler == 'slurm':
+        write_slurm_file(file_name, parameters)
+    elif parameters.scheduler == 'pbs':
+        write_pbs_file(file_name, parameters)
+    else:
+        print('Select job scheduler: slurm / pbs')
 
-    return new_lines
+
+def write_slurm_file(file_name, parameters):
+    """ Write slurm ob submission file """
+    with open(slurm_file, 'r') as sf:
+        job_lines = sf.readlines()
+    job_lines[2] = '#SBATCH --job-name=%s\n' % parameters.name
+    job_lines[3] = '#SBATCH --output=%s.out\n' % parameters.name
+    job_lines[4] = '#SBATCH --nodes=%i\n' % parameters.nodes
+    job_lines[5] = '#SBATCH --ntasks-per-node=%i\n' % parameters.ppn
+    job_lines[6] = '#SBATCH --time=%s\n' % parameters.walltime
+    job_lines[7] = '#SBATCH --cluster=%s\n' % parameters.cluster
+    job_lines[17] = 'mpirun -np ${SLURM_NTASKS} lmp_mpi -in %s > %s\n' % (parameters.input, parameters.output)
+    with open(file_name, 'w') as job_file:
+        for line in job_lines:
+            job_file.write(line)
+
+
+def write_pbs_file(file_name, parameters):
+    """ Write slurm ob submission file """
+    with open(slurm_file, 'r') as sf:
+        job_lines = sf.readlines()
+    job_lines[3] = '#PBS -N %s\n' % parameters.name
+    job_lines[4] = '#PBS -q %s\n' % parameters.queue
+    job_lines[5] = '#PBS -l nodes=%i:ppn=%i\n' % (parameters.nodes, parameters.ppn)
+    job_lines[6] = '#PBS -l walltime=%s\n' % parameters.walltime
+    job_lines[15] = 'prun lammps < %s > %s' % (parameters.input, parameters.output)
+    with open(file_name, 'w') as job_file:
+        for line in job_lines:
+            job_file.write(line)
 
 
 def add_run_info(run_info, run_dir):
