58b1817a4e59a6aea2f01a1c8cf761ad3584cda1
kbsezginel <kbs37@pitt.edu>
2017-09-21 14:04:15 -0400

    Separate parameters into yaml files.

--- thermof/parameters/__init__.py
+++ thermof/parameters/__init__.py
@@ -1,2 +1,17 @@
+import os
+import yaml
 from .parameters import Parameters
-from .parameters import k_parameters, plot_parameters, lammps_parameters
+
+
+def read_yaml(yaml_file):
+    """ Read given yaml file """
+    with open(yaml_file, 'r') as f:
+        var = yaml.load(f)
+    return var
+
+
+par_dir = os.path.abspath(os.path.dirname(__file__))
+
+k_parameters = read_yaml(os.path.join(par_dir, 'k_parameters.yaml'))
+plot_parameters = read_yaml(os.path.join(par_dir, 'plot_parameters.yaml'))
+lammps_parameters = read_yaml(os.path.join(par_dir, 'lammps_parameters.yaml'))
