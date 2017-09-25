9d0a47273fbd282960e7c2dc10e70313ff1d9752
kbsezginel <kbs37@pitt.edu>
2017-09-25 14:40:41 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="thermof",
     version="0.1.2",
-    description="Thermoelectrically entangled MOFs",
+    description="Investigating thermal conductivity of MOFs using Lammps",
     author="Kutay B. Sezginel",
     author_email="kbs37@pitt.edu",
     install_requires=['pytest',
@@ -12,7 +12,7 @@ setup(
                       'matplotlib',
                       'periodictable',
                       'ase'],
-    dependency_links=['git+ssh://git@github.com/kbsezginel/lammps_interface.git']
+    dependency_links=['git+ssh://git@github.com/kbsezginel/lammps_interface.git'],
     include_package_data=True,
     packages=find_packages()
 )
