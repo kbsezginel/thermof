61870c0ff292892445cf9eb8e94acb03e69d5ae3
kbsezginel <kbs37@pitt.edu>
2017-09-26 14:41:36 -0400

    Add new thermo headers and add import.

--- thermof/parameters/__init__.py
+++ thermof/parameters/__init__.py
@@ -1,2 +1,3 @@
 from .parameters import Parameters
-from .default import k_parameters, plot_parameters, lammps_parameters, thermof_parameters, job_parameters
+from .default import k_parameters, plot_parameters, lammps_parameters, job_parameters
+from .default import thermof_parameters, thermo_headers
