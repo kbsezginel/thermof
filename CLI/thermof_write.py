1d7a3aee8ab9826d63969243a9799acc8ec3b8fc
kbsezginel <kbs37@pitt.edu>
2017-11-15 13:26:13 -0500

    Add multiple run initialization for CLI(write).

--- CLI/thermof_write.py
+++ CLI/thermof_write.py
@@ -38,6 +38,8 @@ parser.add_argument('molecule', type=str, help='Molecule file to read.')
 # Optional arguments
 parser.add_argument('--forcefield', '-ff', default='UFF', type=str, metavar='',
                     help='Force field for molecule file.')
+parser.add_argument('--runs', '-r', default=1, type=int, metavar='',
+                    help='Force field for molecule file.')
 
 # Parse arguments
 args = parser.parse_args()
@@ -48,6 +50,9 @@ sim = Simulation(mof=args.molecule, parameters=simpar)
 mof_name = os.path.splitext(os.path.basename(args.molecule))[0]
 sim.simdir = os.path.join(os.path.dirname(args.molecule), mof_name)
 try:
-    sim.initialize()
+    if args.runs == 1:
+        sim.initialize()
+    elif args.runs > 1:
+        sim.initialize_runs(args.runs)
 except Exception as e:
     print(e)
