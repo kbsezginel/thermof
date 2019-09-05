341e92d6efc9ca21dae5c77fa6c0b7dcd85e4755
kbs37 <kbs37@pitt.edu>
2019-09-04 22:46:06 -0400

    bmof scripts, add runs now only uses file prefixes instead of full file names

--- scripts/breathing_mof/init/add_runs.py
+++ scripts/breathing_mof/init/add_runs.py
@@ -5,6 +5,7 @@ Would create run directories in simdir starting from 2 using 1 as reference.
 """
 import os, sys
 import shutil
+from glob import glob
 from lammps_tools import change_job_name
 
 
@@ -12,12 +13,13 @@ refno = input('Enter reference (start) sim no [1]: ') or '1'
 start_seed = int(input('Enter starting seed no [123456]: ') or '123456')
 nruns = int(input('Enter number of total runs: '))
 job_name = input('Enter job name: ')
+# File name prefixes for glob to find files
+data_file_name = 'data.*'
+in_file_name = 'in.*'
+job_file_name = 'job.*'
 
 simdir = os.path.abspath(sys.argv[1])
 refdir = os.path.join(simdir, refno)
-data_file_name = 'data.breathing_mof'
-in_file_name = 'in.breathing_mof'
-job_file_name = 'job.lammps'
 
 def change_seed(source_input, dest_input, seed=999999):
     """ Change seed number of Lammps input """
@@ -36,8 +38,12 @@ for run in range(int(refno) + 1, int(refno) + nruns):
     rundir = os.path.join(simdir, str(run))
     print('Adding -> %s' % rundir)
     os.makedirs(rundir, exist_ok=True)
-    shutil.copy(os.path.join(refdir, data_file_name), os.path.join(rundir, data_file_name))
+    # Data file
+    data_file = glob(os.path.join(refdir, data_file_name))[0]
+    shutil.copy(data_file, rundir)
     # Change seed number
-    change_seed(os.path.join(refdir, in_file_name), os.path.join(rundir, in_file_name), seed=start_seed + run)
+    in_file = glob(os.path.join(refdir, in_file_name))[0]
+    change_seed(in_file, os.path.join(rundir, os.path.basename(in_file)), seed=start_seed + run)
     # Change job name
-    change_job_name(os.path.join(refdir, job_file_name), os.path.join(rundir, job_file_name), job_name='%s-%i' % (job_name, run))
+    job_file = glob(os.path.join(refdir, job_file_name))[0]
+    change_job_name(job_file, os.path.join(rundir, os.path.basename(job_file)), job_name='%s-%i' % (job_name, run))
